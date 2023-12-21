853d3c55243111d959e3a04cd4fb5ee73f950002
Lorenz Fahrner <lorenz.fahrner@gmail.com>
2023-12-21 16:15:50 +0100

    including post init function

--- ipsuite/configuration_generation/cutout.py
+++ ipsuite/configuration_generation/cutout.py
@@ -7,10 +7,15 @@ import zntrack
 
 class CutoutsFromStructures(ips.base.ProcessSingleAtom):
 
-    structure = self.get_data()
+    structure = zntrack.params()
     central_atom_index: int = zntrack.params(None)
     r_cutoff: float = zntrack.params()
     seed: int = zntrack.params(1)
+
+    def __post_init__(self):
+        self.structure = self.get_data()
+        if self.central_atom_index is None:
+            self.central_atom_index = np.random.randint(len(self.structure))
 
     def _center_wrap(self):
         v = np.array([0.5, 0.5, 0.5]) - self.structure.get_scaled_positions()[self.central_atom_index]
@@ -21,8 +26,6 @@ class CutoutsFromStructures(ips.base.ProcessSingleAtom):
     def run(self):
         np.random.seed(self.seed)
 
-        if self.central_atom_index is None:
-            self.central_atom_index = np.random.randint(len(self.structure))
         
 
 
